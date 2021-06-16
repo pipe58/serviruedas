bc035c0771b58d5dfdc08a73a44a97ee5ecffffa
Jonathan Valdes <jonathan.valdes.o@gmail.com>
2021-06-16 09:02:59 -0400

    Se agrega template para envío de email

--- src/java/Config/TemplateCorreo.java
+++ src/java/Config/TemplateCorreo.java
@@ -13,45 +13,51 @@ public class TemplateCorreo {
     
     public String templateCorrreo(){
         
-        /*
-    <!DOCTYPE html> 
-    <html xmlns="http://www.w3.org/1999/xhtml"> 
-        <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />		 
-		<title>Consulta ServiRuedas</title>		 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>		 
-	</head>
-	<body style="margin: 0; padding: 0;"> 
-		
-            <table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
-
-                    <tr>
-                        <td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0;">
-                                <img src="images/h1.gif" alt="Creating Email Magic" width="300" height="230" style="display: block;" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> Formulario de Contacto </td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 20px 0 30px 0;">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
-                        </td> 
-                    </tr>
-
-                    <tr>
-                        <td bgcolor="#ee4c50" style="padding: 30px 30px 30px 30px;">
-                                Footer
-                        </td>
-                    </tr> 
-
-            </table>
-	</body> 
-</html>
+        String template = "";
         
-        */
-        
-        return "";
+        template = "<!DOCTYPE html> \n" +
+            "<html xmlns=\"http://www.w3.org/1999/xhtml\"> \n" +
+                "<head>\n" +
+                    "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /> \n" +
+                    "<title>Consulta ServiRuedas</title> \n" +
+                    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/> \n" +
+                "</head>\n" +
+                "<body style=\"margin: 0; padding: 0;\"> \n" + "\n" +
+                    "<table align=\"center\" border=\"1\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\">\n" +"\n" +
+                        "<tr>\n" +
+                            "<td align=\"center\" bgcolor=\"#70bbd9\" style=\"padding: 40px 0 30px 0;\">\n" +
+                
+                               // "<img src=\"images/h1.gif\" alt=\"Creating Email Magic\" width=\"300\" height=\"230\" style=\"display: block;\" />\n" +
+                            "</td>\n" +
+                        "</tr>\n" +
+                        "<tr>\n" +
+                            "<td> Formulario de Contacto </td>\n" +
+                        "</tr>\n" +
+                        "<tr>\n" +
+                            "<td style=\"padding: 20px 0 30px 0;\">\n" +
+                                "Mensaje de Consulta del Correo. \n" +
+                            "</td> \n" +
+                        "</tr>\n" + "\n" +
+                        "<tr>\n" +
+                        "<td bgcolor=\"#ee4c50\" style=\"padding: 30px 30px 30px 30px;\">\n" +
+                            "Footer \n" +
+                        "</td>\n" +
+                        "</tr> \n" + "\n" +
+                    "</table>\n" +
+                "</body> \n" +
+            "</html>";
+      
+        return template;
     }
+    
+    
+    public static String latex(String tex) {
+        String url = "http://chart.apis.google.com/chart?cht=tx&chl=" + tex;
+        return "<img style=\"-webkit-user-select: none\" alt=\"ServiRuedas\" width=\"300\" height=\"230\" src=\"" + url + "\"/>";
+    }
+    
+     public static void main (String [] args){
+         System.out.println(latex("ServiRuedas"));
+     }
     
 }
